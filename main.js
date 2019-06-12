f1371780fcfcc357ae84f014c390fc73fc477f3e
kimbo9090 <tomas9080@gmail.com>
2019-06-12 15:34:12 +0200

    Download now works, OnEnd finished

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const url = require('url');
 const path = require('path');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
-const { app, BrowserWindow, Menu, ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg')
+const {
+    app,
+    BrowserWindow,
+    Menu,
+    ipcMain
+} = electron;
 let mainWindow;
 // Create main window
 
@@ -34,12 +40,10 @@ app.on('ready', function () {
 
 
 // Create Menu Template
-const mainMenuTemplate = [
-    {
+const mainMenuTemplate = [{
         label: 'Quit',
         // Different command for different platform
-        accelerator: process.platform == 'darwin' ? 'Command+Q' :
-            'Ctrl+Q',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
         click() {
             app.quit();
         }
@@ -51,11 +55,9 @@ const mainMenuTemplate = [
 if (process.env.NODE_ENV !== 'production') {
     mainMenuTemplate.push({
         label: 'Developer Tools',
-        submenu: [
-            {
+        submenu: [{
                 label: 'Toogle DevTools',
-                accelerator: process.platform == 'darwin' ? 'Command+I' :
-                    'Ctrl+I',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
                 click(item, focusedWindow) {
                     focusedWindow.toggleDevTools();
                 }
@@ -67,16 +69,38 @@ if (process.env.NODE_ENV !== 'production') {
     });
 }
 // Catch url:add
-ipcMain.on('url:add',function(e,url){
-    console.log(url)
-    // We need to handle if it's a playlist
-    ytdl.getInfo(url, (err,info)=> {
-        
-        if (err) throw err;
-        let title = info.title;
-        // If we want audio
-        ytdl('http://www.youtube.com/watch?v=A02s8omM_hI')
-        .pipe(fs.createWriteStream('video.flv'));
-        mainWindow.webContents.send('activate:button');
-    })
+ipcMain.on('url:add', function (e, url, format) {
+    // TODO Handle if it's a playlist
+
+    // Get info of the video for later use
+    ytdl.getInfo(url, (err, info) => {
+        const b = info.length_seconds;
+        console.log(b)
+    });
+    if (format == 'video') {
+        // We download the file in .mp4 format
+        const s = ytdl(url, {
+                filter: (format) => format.container === `mp4`
+            })
+            // We create a data stream for downloading the video, when it's end, we activate the download button
+            .pipe(fs.createWriteStream('videos/' + Date.now() + '.mp4')).on('end', () => { // This will go on until the file process is fully ended
+                mainWindow.webContents.send('activate:button');
+            });
+    } else if (format == 'audio') {
+        const stream = ytdl(url, {
+            filter: (format) => format.container === `mp4`
+        })
+        // Download in .mp4 format and do a conversion to .mp3
+        ffmpeg(stream).audioBitrate('128k').format('mp3').save('videos/' + Date.now() + '.mp3').on('end', () => { // This will go on until the file process is fully ended
+            mainWindow.webContents.send('activate:button');
+        }).on('progress', (progress) => {
+            // Progress bar
+            console.log(b)
+            console.log(progress.timemark);
+            console.log(ytdl.getInfo)
+        });
+
+    }
+
+
 })
